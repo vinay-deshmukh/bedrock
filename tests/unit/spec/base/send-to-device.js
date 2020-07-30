10f02d57ff7eba8ba1103adc3cffed8ebf3fc435
vinay-deshmukh <vinay_deshmukh@outlook.com>
2020-07-30 13:11:55 +0530

    clean up test file

--- tests/unit/spec/base/send-to-device.js
+++ tests/unit/spec/base/send-to-device.js
@@ -22,7 +22,7 @@ describe('send-to-device.js', function() {
                         '<ul class="error-list hidden"></ul>' +
                         '<div class="send-to-device-form-fields">' +
                             '<input type="hidden" value="all">' +
-                            '<label id="form-input-label" for="send-to-device-input" data-alt="Enter your email or 10-digit phone number.">Enter your email.</label>' +
+                            '<label class="form-input-label" for="send-to-device-input" data-alt="Enter your email or 10-digit phone number.">Enter your email.</label>' +
                             '<div class="inline-field">' +
                                 '<input id="send-to-device-input" class="send-to-device-input" type="text" required>' +
                                 '<button type="submit">Send</button>' +
@@ -53,10 +53,8 @@ describe('send-to-device.js', function() {
         $('#send-to-device').remove();
         Mozilla.SendToDevice.COUNTRY_CODE = '';
     });
-    
-    //works
-    describe('instantiation', function() {
 
+    describe('instantiation', function() {
         it('should create a new instance of SendToDevice', function() {
             spyOn(form, 'getLocation');
             spyOn(form, 'bindEvents');
@@ -67,7 +65,6 @@ describe('send-to-device.js', function() {
         });
     });
 
-    //works
     describe('inSupportedCountry', function() {
         it('should be true for countries in data-countries, and false for others', function() {
             Mozilla.SendToDevice.COUNTRY_CODE = 'de';
@@ -92,8 +89,6 @@ describe('send-to-device.js', function() {
         });
 
         it('should call bedrock geo to update the messaging', function() {
-            console.log('getLocation test starts');
-            // spyOn($, 'get').and.callFake(function () {
             spyOn(window, 'fetch').and.callFake(function () {
                 var d = $.Deferred();
                 var data = {
@@ -104,26 +99,14 @@ describe('send-to-device.js', function() {
             });
 
             spyOn(form, 'updateMessaging').and.callThrough();
-            console.log('start form init');
-            
-            // jasmine.clock().install();
-
             form.init();
-            console.log('form init after');
 
             // Wait for the fetch().then() to execute
             jasmine.clock().tick(6000);
-            console.log('6000 ms later...');
 
-            // spyOn($, 'get');
-            // expect($.get).toHaveBeenCalledWith('/country-code.json');
             expect(window.fetch).toHaveBeenCalledWith('/country-code.json');
-            console.log('fetch check done');
             expect(form.updateMessaging).toHaveBeenCalled();
-            console.log('test:' + Mozilla.SendToDevice.COUNTRY_CODE);
             expect(Mozilla.SendToDevice.COUNTRY_CODE).toEqual('us');
-
-            // jasmine.clock().uninstall();
         });
     });
 
@@ -138,7 +121,6 @@ describe('send-to-device.js', function() {
         });
 
         it('should execute the geoCallback function when provided', function() {
-            // spyOn($, 'get').and.callFake(function () {
             spyOn(window, 'fetch').and.callFake(function () {
                 var d = $.Deferred();
                 var data = {
@@ -152,13 +134,13 @@ describe('send-to-device.js', function() {
             spyOn(form, 'geoCallback').and.callThrough();
             form.init();
 
+            // Wait for the fetch().then() to execute
             jasmine.clock().tick(6000);
 
             expect(form.geoCallback).toHaveBeenCalledWith('fr');
         });
 
         it('should execute the geoCallback function when geo lookup fails', function() {
-            // spyOn($, 'get').and.callFake(function () {
             spyOn(window, 'fetch').and.callFake(function () {
                 var d = $.Deferred();
                 d.reject('error');
@@ -169,7 +151,9 @@ describe('send-to-device.js', function() {
             spyOn(form, 'geoCallback').and.callThrough();
             form.init();
 
+            // Wait for the fetch().then() to execute
             jasmine.clock().tick(6000);
+
             expect(form.geoCallback).toHaveBeenCalledWith('');
         });
     });
@@ -185,7 +169,6 @@ describe('send-to-device.js', function() {
         });
 
         it('should call showSMS if users is inside the US', function() {
-            // spyOn($, 'get').and.callFake(function () {
             spyOn(window, 'fetch').and.callFake(function () {
                 var d = $.Deferred();
                 var data = {
@@ -197,13 +180,15 @@ describe('send-to-device.js', function() {
 
             spyOn(form, 'showSMS').and.callThrough();
             form.init();
+
+            // Wait for the fetch().then() to execute
             jasmine.clock().tick(6000);
+
             expect(form.showSMS).toHaveBeenCalled();
             expect($('.send-to-device-form').hasClass('sms-country')).toBeTruthy();
         });
 
         it('should not call showSMS if users is outside a supported country', function() {
-            // spyOn($, 'get').and.callFake(function () {
             spyOn(window, 'fetch').and.callFake(function () {
                 var d = $.Deferred();
                 var data = {
@@ -215,7 +200,10 @@ describe('send-to-device.js', function() {
 
             spyOn(form, 'showSMS').and.callThrough();
             form.init();
+
+            // Wait for the fetch().then() to execute
             jasmine.clock().tick(6000);
+
             expect(form.showSMS).not.toHaveBeenCalled();
         });
     });
@@ -240,15 +228,10 @@ describe('send-to-device.js', function() {
     describe('onFormSubmit', function() {
 
         beforeEach(function() {
-            // jasmine.clock().install();
             this.firstGetCall = function(){
-                // var d = $.Deferred();
                 var data = {
                     country_code: 'us'
                 };
-                // d.resolve(data);
-                console.log('first has executed');
-                // return d.promise();
                 var pd = window.Promise.resolve(
                     new Response(
                         new Blob([JSON.stringify(data)])
@@ -257,193 +240,91 @@ describe('send-to-device.js', function() {
                 return pd;
             };
 
-            // Specifies the delay to use in setTimeout
+            // Specifies the delay to use in setTimeout for `expect` calls
+            //    The `expect` functions need to be run after a slighty delay
+            //    as we need to wait for the Promise chain to finish executing.
+            // Note:
             // Small values(100) work, but might fail depending on the order of how
             // specs are run
             // Higher values(1000) means overall test execution will take slightly longer
             // 500 is a middle ground
             this.timeDelayExpect = 500;
-
         });
 
-        afterEach(function() {
-            // jasmine.clock().uninstall();
-        });
 
         it('should handle success', function(done) {
-            console.log('\n\nshould handle success');
-            // var first = (function() {
-            //     var d = $.Deferred();
-            //     var data = {
-            //         country_code: 'us'
-            //     };
-            //     d.resolve(data);
-            //     console.log('first has executed');
-            //     return d.promise();
-            // })();
             var second = function() {
-                // var d = $.Deferred();
                 var data = {
                     'success': 'success'
-                    // 'success': true
                 };
-                // d.resolve(data);
-                console.log('second has executed');
-                // var pd =  d.promise();
-                
                 var pd = window.Promise.resolve(
                     new Response(
                         new Blob([JSON.stringify(data)])
                     )
                 );
-
-                
-                return pd;//.then((s)=>{return s;});
+                return pd;
             };
-            console.log('before spy on fetch');
-            // spyOn(window, 'fetch').and.returnValues(
-            //     // First call is a GET request
-            //     // first,
-            //     this.firstGetCall(),
-            //     // Second call is a POST request
-            //     second()
-            // );
+
             var fetchSpy = spyOn(window, 'fetch');
             fetchSpy.and.callFake(this.firstGetCall);
-            console.log('spy on fetch done');
-            
 
-            console.log('before: form.onFormSuccess:' + form.onFormSuccess);
             spyOn(form, 'onFormSuccess').and.callThrough();
-            console.log('after:  form.onFormSuccess:' + form.onFormSuccess);
 
             form.init();
-            console.log('after form init');
-
 
             fetchSpy.and.callFake(second);
 
-            var cf = document.querySelector('.send-to-device-form');
-            console.log(cf.constructor.name);
-            console.log(cf.submit);
-            console.log(cf.onsubmit);
+            // requestSubmit() validates the form,
+            // so we need to insert some data in the <input> tag
+            document.getElementById('send-to-device-input').value = 'abc@def.com';
+            document.querySelector('.send-to-device-form').requestSubmit();
 
-            var inputEmailPhone = document.getElementById('send-to-device-input');
-            inputEmailPhone.value = 'abc@def.com';
-
-            try{
-                console.log('try: before hit submit');
-
-                cf.requestSubmit();
-
-                console.log('try: after hit submit');
-            } catch(err){
-                console.log('catch: before err print');
-                console.log(err);
-                console.log('catch: after err print');
-            } finally{
-                console.log('try finally');
-            }
-
-            console.log('submit form done');
-            // expect($.post).toHaveBeenCalled();
-            // expect(window.fetch).toHaveBeenCalled();
-            
 
             setTimeout(function(){
-                console.log('\n\n\nexpecting...');
-                // expect(window.fetch).toHaveBeenCalledTimes(100); // SHOULD FAIL
-                var f = window.fetch.calls.count();
-                console.log('fetch calls count:' + f);
-                expect(window.fetch).toHaveBeenCalledTimes(2); // once for get, once for post
-                expect(form.onFormSuccess).toHaveBeenCalled(); // TODO: remove
+                // once for get, once for post
+                expect(window.fetch).toHaveBeenCalledTimes(2);
                 expect(form.onFormSuccess).toHaveBeenCalledWith('success');
                 done();
-                console.log('done run in expecting');
             }, this.timeDelayExpect);
-
-            console.log('it end');
         });
         
         it('should handle error', function(done) {
-            console.log('\n\n\nshould handle error');
-
-            // spyOn($, 'post').and.callFake(function () {
-            // spyOn(window, 'fetch').and.callFake(function () {
-            //     var d = $.Deferred();
-            //     var data = {
-            //         'errors': 'Please enter an email address.'
-            //     };
-            //     d.resolve(data);
-            //     return d.promise();
-            // });
-            // spyOn(window, 'fetch');.and.returnValues(
-            //     //get
-            //     this.firstGetCall(),
-            //     //post
-            //     (function () {
-            //         var d = $.Deferred();
-            //         var data = {
-            //             'errors': 'Please enter an email address.'
-            //         };
-            //         d.resolve(data);
-            //         return d.promise();
-            //     })()
-            //     );
 
             var fetchSpy = spyOn(window, 'fetch');
             fetchSpy.and.callFake(this.firstGetCall);
-
 
 
             spyOn(form, 'onFormError').and.callThrough();
 
             form.init();
             var second = function() {
-                // var d = $.Deferred();
                 var data = {
                     'errors': 'Please enter an email address.'
                 };
-                // d.resolve(data);
-                console.log('second has executed');
-                // var pd =  d.promise();
-                
                 var pd = window.Promise.resolve(
                     new Response(
                         new Blob([JSON.stringify(data)])
                     )
                 );
-                return pd;//.then((s)=>{return s;});
+                return pd;
             };
             fetchSpy.and.callFake(second);
-            var inputEmailPhone = document.getElementById('send-to-device-input');
-            inputEmailPhone.value = 'abc@def.com';
 
-
-            // $('.send-to-device-form').submit();
+            // requestSubmit() validates the form,
+            // so we need to insert some data in the <input> tag
+            document.getElementById('send-to-device-input').value = 'abc@def.com';
             document.querySelector('.send-to-device-form').requestSubmit();
 
 
-            // expect($.post).toHaveBeenCalled();
-            // expect(window.fetch).toHaveBeenCalled();
             setTimeout(function(){
-                expect(window.fetch).toHaveBeenCalledTimes(2); // once for get, once for post
+                // once for get, once for post
+                expect(window.fetch).toHaveBeenCalledTimes(2);
                 expect(form.onFormError).toHaveBeenCalledWith('Please enter an email address.');
                 done();
             }, this.timeDelayExpect);
-            
-
         });
 
         it('should handle failure', function(done) {
-
-            // spyOn($, 'post').and.callFake(function () {
-            // spyOn(window, 'fetch').and.callFake(function () {
-            //     var d = $.Deferred();
-            //     var error = 'An error occurred in our system. Please try again later.';
-            //     d.reject(error);
-            //     return d.promise();
-            // });
 
             var fetchSpy = spyOn(window, 'fetch');
             fetchSpy.and.callFake(this.firstGetCall);
@@ -452,38 +333,25 @@ describe('send-to-device.js', function() {
 
             form.init();
             var second = function() {
-                // var d = $.Deferred();
                 var error = 'An error occurred in our system. Please try again later.';
-                // d.reject(error);
-                // return d.promise();
-                
                 var pd = window.Promise.reject(
                     error
                 );
-
-                
-                return pd;//.then((s)=>{return s;});
+                return pd;
             };
             fetchSpy.and.callFake(second);
 
-            var inputEmailPhone = document.getElementById('send-to-device-input');
-            inputEmailPhone.value = 'abc@def.com';
-
+            // requestSubmit() validates the form,
+            // so we need to insert some data in the <input> tag
+            document.getElementById('send-to-device-input').value = 'abc@def.com';
             document.querySelector('.send-to-device-form').requestSubmit();
-            // expect($.post).toHaveBeenCalled();
 
-            setTimeout(function(){
+            setTimeout(function() {
+                // only 1 get request
                 expect(window.fetch).toHaveBeenCalled();
                 expect(form.onFormFailure).toHaveBeenCalledWith('An error occurred in our system. Please try again later.');
-
-                var fs = form.onFormFailure.calls.allArgs();
-                console.log('form.onFormFailure args:' + fs);
-
-                console.log('\n\nexpect failure done');
                 done();
             }, this.timeDelayExpect);
-
-            
         });
         
     });
